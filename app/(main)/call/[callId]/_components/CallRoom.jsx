5bec9f1a5f8ff104d5bdde6b9a1d192fb3b3dc32
ishuklaji <ishuklaji100@gmail.com>
2026-05-30 03:29:12 +0200

    appointment and feedback page done

---
+++ app/(main)/call/[callId]/_components/CallRoom.jsx
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+
+const CallRoom = ({
+  callId,
+  token,
+  apiKey,
+  currentUser,
+  booking,
+  isInterviewer,
+}) => {
+  return <div></div>;
+};
+
+export default CallRoom
